e4a67112b8984a67a05ec91c7d154822ac8ae2f9
Sauliusm11 <88769620+Sauliusm11@users.noreply.github.com>
2025-05-05 17:23:59 +0300

    Moved out CreateScoreDtoValidator

--- BackEnd/BackEnd.Domain/Data/Dtos/ScoringDtos.cs
+++ BackEnd/BackEnd.Domain/Data/Dtos/ScoringDtos.cs
@@ -5,14 +5,4 @@ namespace BackEnd.Domain.Data.Dtos
     public record ScoreDto(int id, DateTime DateTime,int points, string Email);
 
     public record CreateScoreDto(string[] answers, string Email);
-
-    public class CreateScoreDtoValidator : AbstractValidator<CreateScoreDto>
-    {
-        public CreateScoreDtoValidator()
-        {
-            RuleFor(dto => dto.Email).NotEmpty().NotNull().EmailAddress().Length(min: 2, max: 255);
-            RuleFor(dto => dto.answers).NotEmpty().NotNull();
-        }
-    }
-
 }
---
+++ BackEnd/BackEnd.Domain/Data/Dtos/Validators/CreateScoreDtoValidator.cs
@@ -0,0 +1,19 @@
+﻿using BackEnd.Data.Dtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEnd.Domain.Data.Dtos.Validators
+{
+    public class CreateScoreDtoValidator : AbstractValidator<CreateScoreDto>
+    {
+        public CreateScoreDtoValidator()
+        {
+            RuleFor(dto => dto.Email).NotEmpty().NotNull().EmailAddress().Length(min: 2, max: 255);
+            RuleFor(dto => dto.answers).NotEmpty().NotNull();
+        }
+    }
+}
